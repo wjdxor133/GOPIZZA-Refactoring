dda50bc146a9a2e8f08dd44f7ccf8858ac6ca244
wjdxor133 <wjdxor1224@gmail.com>
2021-08-02 14:28:02 +0900

    Updates

---
+++ precache-manifest.fa9970552d194a7955c090e91373e6e4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4e79c50a467610e3d4b24ce0a62f03",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/index.html"
+  },
+  {
+    "revision": "748783a40fc3b7073bca",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/css/2.31e5bf2e.chunk.css"
+  },
+  {
+    "revision": "748783a40fc3b7073bca",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/js/2.fdbcb20e.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/js/2.fdbcb20e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d381f77d0ff9ddef2e8b",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/js/main.1edfc535.chunk.js"
+  },
+  {
+    "revision": "b013f242d081dd8ef9be",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/js/runtime-main.f57d76ff.js"
+  },
+  {
+    "revision": "21f53ac939d17cb812817f101870bebd",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/media/gopizza_logo.21f53ac9.jpg"
+  },
+  {
+    "revision": "69e24c88965d792dc153fe584cb8964d",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/media/logo.69e24c88.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "3c2634767cd2a27eb778b32e190e74a9",
+    "url": "/wjdxor133/GOPIZZA-Refactoring/static/media/video_1.3c263476.gif"
+  }
+]);

f092a1cafe2656113608906427edb22c4f070665
wjdxor133 <wjdxor1224@gmail.com>
2021-08-05 17:25:32 +0900

    Design: Hero 컴포넌트 css 수정

--- src/components/Common/Hero/Hero.styles.ts
+++ src/components/Common/Hero/Hero.styles.ts
@@ -38,7 +38,7 @@ export const HeroItems = styled.div`
 `;
 
 export const HeroH1 = styled.h1`
-  font-size: clamp(2.5rem, 10vw, 5rem);
+  font-size: clamp(2.5rem, 10vw, 4rem);
   margin-bottom: 1rem;
   letter-spacing: 3px;
 `;
